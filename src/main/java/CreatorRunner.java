f582460074e52d8240c24529ef3b2eb257b6b8b2
Catrin <95254982+winter7eaf@users.noreply.github.com>
2022-03-24 15:36:30 +0000

    Delete src/main/java directory

--- src/main/java/CreatorRunner.java
+++
@@ -1,7 +0,0 @@
-public class CreatorRunner {
-    public static void main(String[] args) {
-        //Create a new CreatorController that create a PDF and run the programme
-        CreatorController controller = new CreatorController();
-        controller.run();
-    }
-}
